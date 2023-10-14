5eadd7341ded255f72cc0cfc4eb5143454c5110f
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2023-10-14 22:06:21 +0700

    More hotkeys + Fix hotkeys

--- Assets/Scripts/Behaviors/Chartmaker/KeyboardHandler.cs
+++ Assets/Scripts/Behaviors/Chartmaker/KeyboardHandler.cs
@@ -10,6 +10,15 @@ public class KeyboardHandler : MonoBehaviour
     public static KeyboardHandler main;
 
     public KeybindActionList Keybindings = new KeybindActionList {
+        // -------------------------------------------------- General
+        { "GE:PlayPause", new KeybindAction {
+            Category = "General",
+            Name = "Play / Pause",
+            Keybind = new Keybind(KeyCode.Space),
+            Invoke = () => InformationBar.main.ToggleSong(),
+        }},
+
+        // -------------------------------------------------- File
         { "FL:New", new KeybindAction {
             Category = "File",
             Name = "New Song",
@@ -28,6 +37,8 @@ public class KeyboardHandler : MonoBehaviour
             Keybind = new Keybind(KeyCode.S, EventModifiers.Command),
             Invoke = () => Chartmaker.main.StartSaveRoutine(),
         }},
+
+        // -------------------------------------------------- Edit
         { "ED:Cut", new KeybindAction {
             Category = "Edit",
             Name = "Cut",
@@ -58,6 +69,80 @@ public class KeyboardHandler : MonoBehaviour
             Keybind = new Keybind(KeyCode.Y, EventModifiers.Command),
             Invoke = () => Chartmaker.main.Redo(),
         }},
+
+        // -------------------------------------------------- Timeline
+        { "TL:Storyboard", new KeybindAction {
+            Category = "Timeline",
+            Name = "Storyboard",
+            Keybind = new Keybind(KeyCode.Alpha1),
+            Invoke = () => TimelinePanel.main.SetTabMode(TimelineMode.Storyboard),
+        }},
+        { "TL:Timing", new KeybindAction {
+            Category = "Timeline",
+            Name = "Timing",
+            Keybind = new Keybind(KeyCode.BackQuote),
+            Invoke = () => TimelinePanel.main.SetTabMode(TimelineMode.Timing),
+        }},
+        { "TL:Lanes", new KeybindAction {
+            Category = "Timeline",
+            Name = "Lanes",
+            Keybind = new Keybind(KeyCode.Alpha2),
+            Invoke = () => TimelinePanel.main.SetTabMode(TimelineMode.Lanes),
+        }},
+        { "TL:LaneStep", new KeybindAction {
+            Category = "Timeline",
+            Name = "Lane Steps",
+            Keybind = new Keybind(KeyCode.Alpha3),
+            Invoke = () => TimelinePanel.main.SetTabMode(TimelineMode.LaneSteps),
+        }},
+        { "TL:HitObjects", new KeybindAction {
+            Category = "Timeline",
+            Name = "Hit Objects",
+            Keybind = new Keybind(KeyCode.Alpha4),
+            Invoke = () => TimelinePanel.main.SetTabMode(TimelineMode.HitObjects),
+        }},
+
+        // -------------------------------------------------- Picker
+        { "PK:Cursor", new KeybindAction {
+            Category = "Picker",
+            Name = "Cursor",
+            Keybind = new Keybind(KeyCode.E),
+            Invoke = () => PickerPanel.main.SetTabMode(PickerMode.Cursor),
+        }},
+        { "PK:Select", new KeybindAction {
+            Category = "Picker",
+            Name = "Select",
+            Keybind = new Keybind(KeyCode.R),
+            Invoke = () => PickerPanel.main.SetTabMode(PickerMode.Select),
+        }},
+        { "PK:Delete", new KeybindAction {
+            Category = "Picker",
+            Name = "Delete",
+            Keybind = new Keybind(KeyCode.T),
+            Invoke = () => PickerPanel.main.SetTabMode(PickerMode.Delete),
+        }},
+        { "PK:Item1", new KeybindAction {
+            Category = "Picker",
+            Name = "1st Item",
+            Keybind = new Keybind(KeyCode.F),
+            Invoke = () => {
+                var mode = TimelinePanel.main.CurrentMode;
+                     if (mode == TimelineMode.Storyboard) PickerPanel.main.SetTabMode(PickerMode.Timestamp);
+                else if (mode == TimelineMode.Timing) PickerPanel.main.SetTabMode(PickerMode.BPMStop);
+                else if (mode == TimelineMode.Lanes) PickerPanel.main.SetTabMode(PickerMode.Lane);
+                else if (mode == TimelineMode.LaneSteps) PickerPanel.main.SetTabMode(PickerMode.LaneStep);
+                else if (mode == TimelineMode.HitObjects) PickerPanel.main.SetTabMode(PickerMode.NormalHit);
+            }
+        }},
+        { "PK:Item2", new KeybindAction {
+            Category = "Picker",
+            Name = "2nd Item",
+            Keybind = new Keybind(KeyCode.G),
+            Invoke = () => {
+                var mode = TimelinePanel.main.CurrentMode;
+                if (mode == TimelineMode.HitObjects) PickerPanel.main.SetTabMode(PickerMode.CatchHit);
+            }
+        }},
     };
     
     public void Awake() 
@@ -76,7 +161,7 @@ public class KeyboardHandler : MonoBehaviour
         {
             return;
         }
-        if (Event.current.isKey)
+        if (Event.current.type == EventType.KeyDown)
         {
             Keybindings.HandleEvent(Event.current);
         }
