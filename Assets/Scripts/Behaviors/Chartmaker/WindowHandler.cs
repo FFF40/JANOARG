2522faa5124894d0941570cbd5ae55da06293e43
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2024-09-22 21:27:01 +0700

    how do i use win32 api on linux??? 3

--- Assets/Scripts/Behaviors/Chartmaker/WindowHandler.cs
+++ Assets/Scripts/Behaviors/Chartmaker/WindowHandler.cs
@@ -32,7 +32,7 @@ public class WindowHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public bool maximized { get; private set; }
     bool isFullScreen;
 
-    bool framed;
+    bool framed = true;
 
     // TODO: Do something with this variable or remove it
     float clickTime = float.NegativeInfinity;
