bb6932390e6291cf769f832674f4dfdab1db2bdf
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2024-12-21 23:34:51 +0700

    Quick build i guess

---
+++ Assets/Scripts/Editor/QuickBuild.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System;
+
+public class QuickBuild
+{
+    [MenuItem("JANOARG/Quick Build")]
+    public static async void BuildGame ()
+    {
+        string path = Path.Combine(Path.GetDirectoryName(Application.dataPath), "Builds");
+
+        Directory.Delete(path, true);
+        Directory.CreateDirectory(path);
+
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
+            locationPathName = Path.Combine(path, "Chartmaker-win-x86_64/Chartmaker.exe"),
+            target = BuildTarget.StandaloneWindows64
+        });
+        ZipFile.CreateFromDirectory(
+            Path.Combine(path, "Chartmaker-win-x86_64"),
+            Path.Combine(path, "Chartmaker-win-x86_64.zip")
+        );
+
+        BuildPipeline.BuildPlayer(new BuildPlayerOptions() {
+            locationPathName = Path.Combine(path, "Chartmaker-linux-x86_64/Chartmaker.x86_64"),
+            target = BuildTarget.StandaloneLinux64
+        });
+        // TODO: figure out how to automate creating a .tar.gz archive
+        // UnityEngine.Debug.Log(await cmd("/bin/bash", "tar -czvf '" + Path.Combine(path, "Chartmaker-linux-x86_64.tar.gz").Replace("'", "''") + "' '"
+        //     + Path.Combine(path, "Chartmaker-linux-x86_64/").Replace("'", "''")));
+    }
+}
