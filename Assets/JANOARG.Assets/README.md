43540df863dfec51a74390b17bb87ae098a0ef1e
BashhScriptKid <dev@bashh.slmail.me>
2025-08-13 22:09:40 +0800

    Update gitignore applied tree

--- Assets/JANOARG.Assets/README.md
+++
@@ -1,3 +0,0 @@
-Private assets folder for the game JANOARG
-
-Contains copyrighted songs, do not make public
--- Assets/JANOARG/Scripts/Behaviors/Options/Input Types/JudgmentOffsetOptionInput.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class JudgmentOffsetOptionInput : FloatOptionInput
-{
-}
