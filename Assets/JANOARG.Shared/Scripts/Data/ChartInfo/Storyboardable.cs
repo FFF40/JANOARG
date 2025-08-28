813464e923b031a6ed27e2a0581b33f68068b644
BashhScriptKid <dev@bashh.slmail.me>
2025-08-29 01:55:50 +0800

    Add notice

--- Assets/JANOARG.Shared/Scripts/Data/ChartInfo/Storyboardable.cs
+++ Assets/JANOARG.Shared/Scripts/Data/ChartInfo/Storyboardable.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+// TODO: REWRITE THIS ASAP
+
 namespace JANOARG.Shared.Data.ChartInfo
 {
     [Serializable]
