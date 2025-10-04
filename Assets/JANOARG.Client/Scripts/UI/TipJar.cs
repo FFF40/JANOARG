cbd52b6e7dc1d379315c0cb934ad8287f25fe26a
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-10-04 14:43:15 +0700

    Adjust tip jar modal 2

--- Assets/JANOARG.Client/Scripts/UI/TipJar.cs
+++ Assets/JANOARG.Client/Scripts/UI/TipJar.cs
@@ -1,21 +1,22 @@
 using JANOARG.Client.Behaviors.Options.Input_Types;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace JANOARG.Client.UI
 {
     public class TipJar : MonoBehaviour
     {
-        [SerializeField] private OptionButton Kofi;
-        [SerializeField] private OptionButton Patreon;
-        [SerializeField] private OptionButton LiberaPay;
-        [SerializeField] private OptionButton UnityAd;
+        public Button KofiButton;
+        public Button PatreonButton;
+        public Button LiberapayButton;
+        public Button UnityAdButton;
 
         private void Start()
         {
-            Kofi.Action      = () => Application.OpenURL("https://ko-fi.com/duducat");
-            //Patreon.Action = () => Application.OpenURL("");
-            LiberaPay.Action = () => Application.OpenURL("https://en.liberapay.com/ducdat0507");
-            UnityAd.Action   = () => throw new System.NotImplementedException("Unity Ad not installed yet (use another PR)");
+            KofiButton.onClick.AddListener(     () => Application.OpenURL("https://ko-fi.com/duducat"));
+            PatreonButton.onClick.AddListener(  () => Application.OpenURL(""));
+            LiberapayButton.onClick.AddListener(() => Application.OpenURL("https://en.liberapay.com/ducdat0507"));
+            UnityAdButton.onClick.AddListener(  () => throw new System.NotImplementedException("Unity Ad not installed yet (use another PR)"));
         }
         
     }
