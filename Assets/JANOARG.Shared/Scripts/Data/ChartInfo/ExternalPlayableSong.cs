170df34795b2814c17755a277650dd9ecbdfa0fc
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-10-05 17:39:37 +0700

    Move to shared repo

--- Assets/JANOARG.Shared/Scripts/Data/ChartInfo/ExternalPlayableSong.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
-
-public class ExternalPlayableSong : ScriptableObject
-{
-    public PlayableSong Data;
-}
